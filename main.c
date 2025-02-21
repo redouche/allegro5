ea5d238c838e7bac1325da560ee77f686d0faa2c
reda <itsrazenyt@gmail.com>
2025-02-21 10:08:07 +0100

    sandbox project, using mouse and keyboard to draw

--- main.c
+++ main.c
@@ -5,32 +5,76 @@
 
 #define WIDTH 600
 #define HEIGHT 400
+#define MOVE_STEP 10
 
 int main(void) {
     // Déclarations
+    ALLEGRO_DISPLAY* window = NULL;
+    ALLEGRO_EVENT_QUEUE* fifo = NULL;
+    ALLEGRO_EVENT event;
+    al_init_primitives_addon();
+    bool end = false;
+    int x = WIDTH / 2, y = HEIGHT / 2; // Position de départ
 
     // Initialisations
     assert(al_init());
-    ALLEGRO_DISPLAY *ecran = al_create_display(WIDTH, HEIGHT);
-    assert(ecran);
-    al_set_window_title(ecran, "Titre");
+    assert(al_install_keyboard());
+    assert(al_install_mouse());
 
-    // Créations
-    al_init_primitives_addon();
+    // Création des éléments nécessaires
+    window = al_create_display(WIDTH, HEIGHT);
+    assert(window);
+    al_set_window_title(window, "Allegro 5");
+
+    fifo = al_create_event_queue();
+    al_register_event_source(fifo, al_get_display_event_source(window));
+    al_register_event_source(fifo, al_get_keyboard_event_source());
+    al_register_event_source(fifo, al_get_mouse_event_source());
+
+    // Première mise à jour de l'écran
     al_clear_to_color(al_map_rgb(255, 255, 255)); // Fond blanc
-    // Je dessine un R
-    al_draw_line(50, 50, 50, 350, al_map_rgb(0, 0, 0), 10.0);
-    al_draw_line(50, 50, 150, 50, al_map_rgb(0, 0, 0), 10.0);
-    al_draw_line(150, 50, 150, 200, al_map_rgb(0, 0, 0), 10.0);
-    al_draw_line(150, 200, 50, 200, al_map_rgb(0, 0, 0), 10.0);
-    al_draw_line(50, 200, 150, 350, al_map_rgb(0, 0, 0), 10.0);
+    al_draw_filled_rectangle(x - 10, y - 10, x + 10, y + 10, al_map_rgb(0, 255, 0)); // Tête verte au départ
     al_flip_display();
 
-    // Fonctionnalités
-    al_rest(5);
+    // Boucle du jeu
+    while (!end) {
+        al_wait_for_event(fifo, &event);
+        switch (event.type) {
+            case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                printf("Fermeture de la fenetre.\n");
+                end = true;
+                break;
 
-    // Libérations
-    al_destroy_display(ecran);
+            case ALLEGRO_EVENT_KEY_DOWN:
+                al_draw_filled_rectangle(x - 10, y - 10, x + 10, y + 10, al_map_rgb(0, 0, 255));
+
+                // Mise à jour de la position
+                if (event.keyboard.keycode == ALLEGRO_KEY_Z) {
+                    y -= MOVE_STEP;
+                } else if (event.keyboard.keycode == ALLEGRO_KEY_Q) {
+                    x -= MOVE_STEP;
+                } else if (event.keyboard.keycode == ALLEGRO_KEY_S) {
+                    y += MOVE_STEP;
+                } else if (event.keyboard.keycode == ALLEGRO_KEY_D) {
+                    x += MOVE_STEP;
+                }
+
+                al_draw_filled_rectangle(x - 10, y - 10, x + 10, y + 10, al_map_rgb(0, 255, 0));
+
+                al_flip_display();
+                break;
+            case ALLEGRO_EVENT_MOUSE_AXES:
+                al_draw_filled_circle(event.mouse.x, event.mouse.y, 5, al_map_rgb(255, 0, 0)); // Cercle rouge visible
+                al_flip_display();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Libération de la mémoire
+    al_destroy_display(window);
+    al_destroy_event_queue(fifo);
 
     return 0;
 }
